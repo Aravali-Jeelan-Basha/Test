bca1efdb254cc90b7c99c6e84228bb2a36e9955b
Aravali-Jeelan-Basha <119558879+Aravali-Jeelan-Basha@users.noreply.github.com>
2025-01-18 16:52:13 +0530

    Add files via upload

---
+++ Day5.js
@@ -0,0 +1,14 @@
+import {Component} from "react"
+import "./components/Day5clrs.css"
+
+class Day5 extends Component{
+    render(){
+        return (
+            <>
+              <p className="name">This is a class Component </p>
+            </>
+        )
+
+    }
+}
+export default Day5;
